08f9221c183bd91e22713ed729080ec7eb6a1af3
Mateus Andrade <mat.andrade@live.com>
2019-03-02 11:48:00 -0300

    RN0.58 working in iOS

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,30 +1,12 @@
-import { Navigation, ScreenVisibilityListener } from 'react-native-navigation';
-
-// import { setCustomText, setCustomView } from 'react-native-global-props';
+import { Navigation } from 'react-native-navigation';
 
 import Home from './App';
 
-// const customTextProps = {
-//   style: {
-//     fontFamily: 'Helvetica Neue'
-//   }
-// };
-
 export function registerScreens(store: {}, Provider: {}) { //eslint-disable-line
-  // setCustomText(customTextProps);
-  Navigation.registerComponent('home', () => Home, store, Provider);
+  Navigation.registerComponentWithRedux(
+    'home',
+    () => Home,
+    Provider,
+    store
+  );
 }
-
-// export function registerScreenVisibilityListener() {
-//   new ScreenVisibilityListener({
-//     // willAppear: ({ screen }) =>,
-//     didAppear: ({
-//       screen, startTime, endTime, commandType
-//     }) => {
-//       const screenName = screen.split('.');
-//       return trackPage(screenName[2]);
-//     },
-//     // willDisappear: ({ screen }) => console.log(`Screen will disappear ${screen}`),
-//     // didDisappear: ({ screen }) => console.log(`Screen disappeared ${screen}`)
-//   }).register();
-// }
